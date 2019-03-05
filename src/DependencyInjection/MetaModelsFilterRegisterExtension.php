4eb9b1833eb4c6cf08b104eb159c1bc7c972b0fd
Mini Model <minimodel@metamodel.me>
2019-03-05 20:47:07 +0100

    Update PHPDoc file headers

--- src/DependencyInjection/MetaModelsFilterRegisterExtension.php
+++ src/DependencyInjection/MetaModelsFilterRegisterExtension.php
@@ -3,17 +3,16 @@
 /**
  * This file is part of MetaModels/filter_register.
  *
- * (c) 2012-2018 The MetaModels team.
+ * (c) 2012-2019 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
  * This project is provided in good faith and hope to be usable by anyone.
  *
- * @package    MetaModels
- * @subpackage FilterRegister
+ * @package    MetaModels/filter_register
  * @author     David Molineus <david.molineus@netzmacht.de>
- * @copyright  2012-2018 The MetaModels team.
+ * @copyright  2012-2019 The MetaModels team.
  * @license    https://github.com/MetaModels/filter_register/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
